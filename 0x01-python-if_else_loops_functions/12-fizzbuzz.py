898c1b285f2febd312e9f6429472939155822331
Ermi277 <mr.ermiyas@gmail.com>
2022-08-23 19:04:27 +0000

    task12comp

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/python3
+def fizzbuzz():
+    for num in range(1, 10):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif num % 3 == 0:
+            print("Fizz ", end="")
+        elif num % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
