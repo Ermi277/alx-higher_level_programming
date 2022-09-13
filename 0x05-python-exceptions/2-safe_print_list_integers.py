4799e2dee926ae380bfed6a2f900d77df1374004
Ermi277 <mr.ermiyas@gmail.com>
2022-09-13 11:55:46 +0000

    task2comp

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), echo="")
             count +=1
-        except:
+        except (valueIndex, TypeIndex):
             continue
     print("")
     return (count)
