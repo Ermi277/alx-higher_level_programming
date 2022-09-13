549f2591e68863c8b0355766cce672ca84bacd23
Ermi277 <mr.ermiyas@gmail.com>
2022-09-13 13:42:24 +0000

    task4fixerr

--- 0x05-python-exceptions/test/main.py
+++ 0x05-python-exceptions/test/main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
-safe_print_division = __import__('3-safe_print_division').safe_print_division
+list_division = __import__('4-list_division').list_division
+my_l_1 = [10, 8, 4]
+my_l_2 = [2, 4, 4]
 
-a = 10
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
+print("--")
+
+my_l_1 = [10, 8, 4, 4]
+my_l_2 = [2, 0, "H", 2, 7]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
