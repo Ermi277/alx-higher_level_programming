a8466ba7fdd0bd2ecf45993be9f47f5f347068c9
Ermi277 <mr.ermiyas@gmail.com>
2022-09-16 06:09:28 +0000

    errorfix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,12 +2,10 @@
 
 """Define a class Square."""
 
-
 class Square:
 
 
     """Represent a square."""
-
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,6 +5,7 @@
 class Square:
 
     """Represent a square."""
+
     def __init__(self, size=0):
         self.__size = size
 
@@ -16,7 +17,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >=0")
-        self.__size = size
+        else:
+            self.__size = size
 
     def area(self):
         return (self.__size * self.__size)
