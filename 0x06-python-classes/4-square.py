f2236fd35fe85fcdd55529e06a5deb21e83d2db7
Ermi277 <mr.ermiyas@gmail.com>
2022-09-16 12:42:33 +0000

    bugfix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,7 +13,7 @@ class Square:
         return self.size
 
     def size(self, value):
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >=0")
