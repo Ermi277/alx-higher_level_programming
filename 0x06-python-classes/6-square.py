207cbf3a427fcda287bd77cd26b676325e1db0d4
Ermi277 <mr.ermiyas@gmail.com>
2022-09-16 13:18:11 +0000

    task6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,11 +8,11 @@ class Square:
     """Represent a square."""
 
     def __init__(self, size=0, position=(0, 0)):
-        self.position = position
-        self.size = size
+        self._position = position
+        self._size = size
 
     def size(self):
-        return (self.size)
+        return (self._size)
 
     def size(self, value):
         if not isinstance(value, int):
@@ -20,25 +20,25 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >=0")
         else:
-            self.size = value
+            self._size = value
 
     def area(self):
-        return (self.size * self.size)
+        return (self._size * self._size)
     
     def my_print(self):
-        for i in range(0, self.size):
-            [print(" ", end="") for j in range(0, self.position[0])]
-            [print("#", end="") for k in range(self.size)]
+        for i in range(0, self._size):
+            [print(" ", end="") for j in range(0, self._position[0])]
+            [print("#", end="") for k in range(self._size)]
             print(" ")
         if self.size == 0:
             print("")
     
     def position(self):
-        return (self.position)
+        return (self._position)
 
     def position(self, value):
         if not isinstance(position, tuple) or len(value) !=2 or \
                 not all(isinstance(num, int) for num in value) \
                 or not all(num >=0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.position = value
+        self._position = value
