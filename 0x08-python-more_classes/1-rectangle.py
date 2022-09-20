b36b847138feba5a8db85b7860d839bc42df88c8
Ermi277 <mr.ermiyas@gmail.com>
2022-09-20 07:29:04 +0000

    task1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,25 +7,25 @@ class Rectangle:
     """Represent a class Rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     def width(self):
-        return self.width
+        return self.__width
 
     def width(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError('width must be an integer')
         elif value < 0:
-            raise ValueError("width must be >=0")
-        self.width = value
+            raise ValueError('width must be >=0')
+        self.__width = value
 
     def height(self):
-        return self.height
+        return self.__height
     
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+            raise TypeError('height must be an integer')
         elif value < 0:
-            raise ValueError("height must be >=0")
-        slef.height = height
+            raise ValueError('height must be >=0')
+        slef.__height = height
