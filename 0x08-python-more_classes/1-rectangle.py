4949e45417699249a5e8cf0af5b5b7a9a9180453
Ermi277 <mr.ermiyas@gmail.com>
2022-09-20 07:01:21 +0000

    task1comp

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
     """Represent a class Rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.width = widt
+        self.width = width
         self.height = height
 
     def width(self):
