15dca2ef539a8fbe7d26afaac543b8ab00f3bd88
Ermi277 <mr.ermiyas@gmail.com>
2022-10-07 15:52:55 +0000

    task3fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -59,7 +59,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("y must be an integer")
         if y < 0:
             raise ValueError("y must be >= 0")
