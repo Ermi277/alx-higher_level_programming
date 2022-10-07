2cbe96246311b853df0232af723525df3fe34223
Ermiyas <mr.ermiyas@gmail.com>
2022-10-07 12:35:23 +0000

    task2correc

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,6 +9,19 @@ class Rectangle(Base):
     """Represent a class Rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle
+
+        Args:
+            width (int): The width of the new Rectangle
+            height (int): The height of the new Rectangle
+            x (int): The x diamension of the new rectangle
+            y (int): The y coordinate of the new Rectangle
+            id (int): The identity of the new Rectangle
+        
+        Raises:
+            TypeError: Raise type error if width, height, x or y is not an int
+            ValueError: Raise error if width, height, x or y < 0
+        """
         self.width = width
         self.height = height
         self.x = x
