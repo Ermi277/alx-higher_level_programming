cdafe44ea52542ff2b8a495854c3b8b3cb8b4699
Ermiyas <mr.ermiyas@gmail.com>
2022-10-07 13:12:16 +0000

    task2correc

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,7 +19,7 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        return (self.__width)
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -31,7 +31,7 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        return (self.__height)
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -43,7 +43,7 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        return (self.__x)
+        return self.__x
     
     @x.setter
     def x(self, value):
@@ -55,7 +55,7 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        return (self.__y)
+        return self.__y
 
     @y.setter
     def y(self, value):
