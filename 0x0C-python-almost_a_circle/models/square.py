56d454df82f78cdddb3ae12f27e32f396b128b42
Ermi277 <mr.ermiyas@gmail.com>
2022-10-08 19:19:55 +0000

    task11:x

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,6 +21,6 @@ class Square(Rectangle):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value <= 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
